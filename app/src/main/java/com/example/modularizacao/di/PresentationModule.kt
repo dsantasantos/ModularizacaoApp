6b67c9999de7d9050f0d6dfef8d99450371fd8c1
Heider Lopes <heider.lopes@b2wdigital.com>
2019-12-05 12:38:04 -0300

    Integracao das camadas

--- app/src/main/java/com/example/modularizacao/di/PresentationModule.kt
+++ app/src/main/java/com/example/modularizacao/di/PresentationModule.kt
@@ -1,11 +1,18 @@
 package com.example.modularizacao.di
 
+import com.example.modularizacao.MainListAdapter
 import com.example.modularizacao.MainViewModel
+import com.squareup.picasso.Picasso
 import io.reactivex.android.schedulers.AndroidSchedulers
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val presentationModule = module {
+
+
+    single { Picasso.get() }
+
+    factory { MainListAdapter(picasso = get()) }
 
     viewModel { MainViewModel(
         useCase = get(),
