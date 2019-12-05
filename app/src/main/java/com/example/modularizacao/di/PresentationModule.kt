74279ca48834517e88164d6bac0e87b203cc0385
Logon Aluno <logonaluno@local.com>
2019-12-05 20:46:12 -0300

    Novo itens aula 05/12/2019

--- app/src/main/java/com/example/modularizacao/di/PresentationModule.kt
+++ app/src/main/java/com/example/modularizacao/di/PresentationModule.kt
@@ -1,15 +1,26 @@
 package com.example.modularizacao.di
 
+import com.example.modularizacao.MainListAdapter
 import com.example.modularizacao.MainViewModel
+import com.squareup.picasso.Picasso
 import io.reactivex.android.schedulers.AndroidSchedulers
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val presentationModule = module {
 
-    viewModel { MainViewModel(
-        useCase = get(),
-        uiScheduler = AndroidSchedulers.mainThread()
-    )
+    single {
+        Picasso.get()
+    }
+
+    factory {
+        MainListAdapter(picasso = get())
+    }
+
+    viewModel {
+        MainViewModel(
+            useCase = get(),
+            uiScheduler = AndroidSchedulers.mainThread()
+        )
     }
 }
